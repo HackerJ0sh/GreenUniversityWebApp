370fb166e51e1311c9d1b6041d96a6d73143ba0d
OperianoECO <potomaya1214@gmail.com>
2024-01-05 21:53:37 +0800

    Update __init__.py
    Now able to run properlyy

--- __init__.py
+++ __init__.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+
+app = Flask(__name__, template_folder='customerTemplates')
 
 
 @app.route('/')
-def cust_homepage():
+def homepage():
     return render_template('customerHomepage.html')
 
 
 if __name__ == '__main__':
     app.run()
+
+
+
