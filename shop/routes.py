dd8701ed7f8f73915c8179c64adca0da6583bec1
bingtai20 <taibing20@gmail.com>
2024-01-08 00:12:02 +0800

    2

--- shop/routes.py
+++ shop/routes.py
@@ -5,3 +5,10 @@ from shop import app,db
 @app.route('/')
 def home():
     return "Home page of your shop"
+
+
+
+
+@app.route('/register')
+def register():
+    return render_template('admin/register.html',title="Register user")
