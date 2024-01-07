954c52753028c23dd55dab3158eb00aaaf8bc033
bingtai20 <taibing20@gmail.com>
2024-01-08 02:34:42 +0800

    456

--- shop/admin/routes.py
+++ shop/admin/routes.py
@@ -1,23 +1,45 @@
 from flask import render_template, session, request, redirect, url_for, flash
 
 from shop import app, db, bcrypt
-from .forms import RegistrationForm
+from .forms import RegistrationForm, LoginForm
 from .models import User
+from shop.products.models import Addproduct,Brand, Category
 import os
 
-@app.route('/')
+
+
+@app.route('/admin')
 def home():
-    return render_template('admin/index.html',title='Admin Page')
+    if 'email' not in session:
+        flash(f'Please Login first','danger')
+        return redirect(url_for('login'))
+    produts = Addproduct.query.all()
+    return render_template('admin/index.html',title='Admin Page', products=products)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    form = RegistrationForm(request.form)
-    if request.method == 'POST' and form.validate():
+    form = RegistrationForm()
+    if form.validate_on_submit():
         hash_password = bcrypt.generate_password_hash(form.password.data)
         user = User(name=form.name.data,username=form.username.data, email=form.email.data,
                     password=hash_password)
         db.session.add(user)
+        flash(f'welcome {form.name.data} Thanks for registering','success')
         db.session.commit()
-        flash(f'Welcome {form.name.data} Thanks for registering','success')
-        return redirect(url_for('home'))
-    return render_template('admin/register.html', form=form, title="Registeration page")
+        return redirect(url_for('login'))
+    return render_template('admin/register.html',title='Register user', form=form)
+
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            session['email'] = form.email.data
+            flash(f'welcome {form.email.data} you are logged in now','success')
+            return redirect(url_for('admin'))
+        else:
+            flash(f'Wrong email and password', 'success')
+            return redirect(url_for('login'))
+    return render_template('admin/login.html',title='Login page',form=form)
