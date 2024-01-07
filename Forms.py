c8b3d43855cb8b4da244a3e63740671d8253e976
bingtai20 <taibing20@gmail.com>
2024-01-08 03:36:10 +0800

    restartrestartrestart

--- Forms.py
+++ Forms.py
@@ -2,10 +2,10 @@ from wtforms import Form, StringField, RadioField, SelectField, TextAreaField, v
 from wtforms.fields import EmailField, DateField
 
 class CreateUserForm(Form):
-    first_name = StringField('First Name', [validators.Length(min=1, max=150), validators.DataRequired()])
-    last_name = StringField('Last Name', [validators.Length(min=1, max=150), validators.DataRequired()])
-    gender = SelectField('Gender', [validators.DataRequired()], choices=[('', 'Select'), ('F', 'Female'), ('M', 'Male')], default='')
-    membership = RadioField('Membership', choices=[('F', 'Fellow'), ('S', 'Senior'), ('P', 'Professional')], default='F')
+    first_name = StringField('Product Name', [validators.Length(min=1, max=150), validators.DataRequired()])
+    last_name = StringField('Product Price', [validators.Length(min=1, max=150), validators.DataRequired()])
+    gender = SelectField('Category', [validators.DataRequired()], choices=[('', 'Select'), ('Shirt', 'Shirt'), ('Pants', 'Pants')], default='')
+    membership = RadioField('Size', choices=[('S', 'Small'), ('M', 'Medium'), ('L', 'Large')], default='S')
     remarks = TextAreaField('Remarks', [validators.Optional()])
 
 class CreateCustomerForm(Form):
