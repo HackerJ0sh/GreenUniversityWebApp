395277e15775ecd9f8584150831a6b8a7b343b83
HackerJ0sh <137512352+HackerJ0sh@users.noreply.github.com>
2024-01-08 16:48:24 +0800

    Updated Payment WTforms class

--- PaymentOtpForm.py
+++ PaymentOtpForm.py
@@ -1,9 +1,9 @@
 from wtforms import Form, validators, IntegerField
 
 class CreatePaymentOtpForm(Form):
-    OTP_code_1 = IntegerField([validators.length(max=1), validators.DataRequired()])
-    OTP_code_2 = IntegerField([validators.length(max=1), validators.DataRequired()])
-    OTP_code_3 = IntegerField([validators.length(max=1), validators.DataRequired()])
-    OTP_code_4 = IntegerField([validators.length(max=1), validators.DataRequired()])
-    OTP_code_5 = IntegerField([validators.length(max=1), validators.DataRequired()])
-    OTP_code_6 = IntegerField([validators.length(max=1), validators.DataRequired()])
+    OTP_code_1 = IntegerField('',[validators.length(max=1), validators.DataRequired()])
+    OTP_code_2 = IntegerField('',[validators.length(max=1), validators.DataRequired()])
+    OTP_code_3 = IntegerField('',[validators.length(max=1), validators.DataRequired()])
+    OTP_code_4 = IntegerField('',[validators.length(max=1), validators.DataRequired()])
+    OTP_code_5 = IntegerField('',[validators.length(max=1), validators.DataRequired()])
+    OTP_code_6 = IntegerField('',[validators.length(max=1), validators.DataRequired()])
