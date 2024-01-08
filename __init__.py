031f9e6e28b94b5983bb03c49107e02acef9055e
OperianoECO <potomaya1214@gmail.com>
2024-01-08 22:46:52 +0800

    Completed CRUD
    CRUD completed, still need to fix image and fix up the UI but i'll do it before week 15

--- __init__.py
+++ __init__.py
@@ -54,24 +54,24 @@ def retrieve_blogs():
 
     return render_template('allBlogs.html', count=len(blogs_list), blogs_list=blogs_list)
 
-@app.route('/updateUser/<int:id>/', methods=['GET', 'POST'])
+@app.route('/updateBlog/<int:id>/', methods=['GET', 'POST'])
 def update_blog(id):
     update_blog_form = CreateBlogForm(request.form)
     if request.method == 'POST' and update_blog_form.validate():
         blogs_dict = {}
-        db = shelve.open('blogs.db', 'r')
+        db = shelve.open('blogs.db', 'w')
         blogs_dict = db['Blogs']
 
         blog = blogs_dict.get(id)
-        update_blog_form.post_name.data = blog.get_first_name()
-        update_blog_form.image.data = blog.get_last_name()
-        update_blog_form.post_content.data = blog.get_gender()
-        update_blog_form.category.data = blog.get_membership()
+        blog.set_post_name(update_blog_form.post_name.data)
+        blog.set_image(update_blog_form.image.data)
+        blog.set_post_content(update_blog_form.post_content.data)
+        blog.set_category(update_blog_form.category.data)
 
-        blogs_dict = db['Blogs']
+        db['Blogs'] = blogs_dict
         db.close()
 
-        return redirect(url_for('retrieve_users'))
+        return redirect(url_for('retrieve_blogs'))
     else:
         blogs_dict = {}
         db = shelve.open('blogs.db', 'r')
@@ -79,12 +79,29 @@ def update_blog(id):
         db.close()
 
         blog = blogs_dict.get(id)
-        update_blog_form.post_name.data = blog.get_first_name()
-        update_blog_form.image.data = blog.get_last_name()
-        update_blog_form.post_content.data = blog.get_gender()
-        update_blog_form.category.data = blog.get_membership()
+        update_blog_form.post_name.data = blog.get_post_name()
+        update_blog_form.image.data = blog.get_image()
+        update_blog_form.post_content.data = blog.get_post_content()
+        update_blog_form.category.data = blog.get_category()
+
+        return render_template('updateBlog.html', form=update_blog_form)
+
+
+@app.route('/deleteBlog/<int:id>', methods=['POST'])
+def delete_blog(id):
+    blogs_dict = {}
+    db = shelve.open('blogs.db', 'w')
+    blogs_dict = db['Blogs']
+    blogs_dict.pop(id)
+    db['Blogs'] = blogs_dict
+    db.close()
+
+    return redirect(url_for('retrieve_blogs'))
+
+
 
 
 if __name__ == '__main__':
     app.run()
+
 
