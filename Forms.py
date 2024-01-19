6f4431a683b4289be86bcb608894751f8aaa823f
bingtai20 <taibing20@gmail.com>
2024-01-19 16:43:25 +0800

    next 1

--- Forms.py
+++ Forms.py
@@ -5,7 +5,7 @@ import random
 
 class CreateUserForm(Form):
     first_name = StringField('Product Name', [validators.Length(min=1, max=150), validators.DataRequired()])
-    last_name = DecimalField('Product Price (please input $ unit)', [validators.DataRequired()])
+    last_name = DecimalField('Product Price', [validators.DataRequired()])
     gender = SelectField('Category', [validators.DataRequired()], choices=[('', 'Select'), ('Shirt', 'Shirt'), ('Pants', 'Pants')], default='')
     membership = RadioField('Size', choices=[('S', 'Small'), ('M', 'Medium'), ('L', 'Large')], default='S')   
     remarks = TextAreaField('Remarks',[validators.Optional()],)
